22aecabff7b1643061f49d957a2aa9e67a343b17
Aldo Matias Lazo Rocha <aldomatias.lazo@uvic.cat>
2022-10-14 16:09:22 +0200

    Implemented Runnable instead of exented Thread

---
+++ app/src/main/java/cat/uvic/teknos/m09/threadtests/ImportantStuff2.java
@@ -0,0 +1,15 @@
+package cat.uvic.teknos.m09.threadtests;
+
+public class ImportantStuff2 implements Runnable{
+    @Override
+    public void run() {
+        try {
+            Thread.sleep(1000);
+            System.out.println("Done important stuff 2");
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+
+}
---
+++ app/src/main/java/cat/uvic/teknos/m09/threadtests/ImportantStuff3.java
@@ -0,0 +1,15 @@
+package cat.uvic.teknos.m09.threadtests;
+
+public class ImportantStuff3 implements Runnable{
+    @Override
+    public void run() {
+        try {
+            Thread.sleep(2*1000);
+            System.out.println("Done important stuff 3");
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+
+}
